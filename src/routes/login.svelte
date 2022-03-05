7d0ffd014067566f876783846342abb732e12627
kharis <kharis.hidayatullah@gmail.com>
2022-03-06 02:13:29 +0700

    test

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
   import { GoogleAuth, FacebookAuth } from '@beyonk/svelte-social-auth'
 </script>
 
@@ -6,4 +6,4 @@
   clientId="your-google-auth-client-id"
   on:auth-success={(e) => console.dir(e.detail.user)}
 />
-<FacebookAuth appId="your-facebook-app-id" on:auth-success={(e) => console.dir(e.detail.user)} />
+<FacebookAuth appId="your-facebook-app-id" on:auth-success={(e) => console.dir(e.detail.user)} /> -->
