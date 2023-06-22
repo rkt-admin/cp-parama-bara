b7c0ef8046e6c7e2bfe3cd9c1d515fc7ac67b979
Rakit.id Admin <admin@rakit.id>
2023-06-22 23:54:24 +0700

    upgrade minor

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 /** @type {import('vite').UserConfig} */
 const config: UserConfig =  {
+  logLevel: 'info',
   plugins: [sveltekit()],
   // allows vite access to ./posts
   server: {
     fs: {
-      allow: ['./']
+      allow: ['./'],
+      strict: false
     }
   },
   resolve: {
